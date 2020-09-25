d4a46e2fcb8855d01662c38a4b19c510f6322d52
lqwangxg <lqwangxg@gmail.com>
2020-09-25 05:01:29 +0000

    commit

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,5 @@
-docker logs $container_name | tail -n13 | grep error
-if [ $? = 1 ]; then 
-  if [ -z $app_name ]; then 
-    tag_name=typenode
-  else
-    tag_name=`echo "$app_name" | awk -F / '{print $3 }'`
-  fi
-  echo "source build succeeded!. tag_name=$tag_name"
-  docker build -t "lqwangxg/$tag_name" -f Dockerfile.deploy .
-else 
-  echo "source build error, stoped. "
-fi
+app_name=`pwd`
+tag_name=`echo "$app_name" | awk -F / '{print $3 }'`
+
+echo "$app_name deploy start. tag_name=$tag_name"
+docker build -t "lqwangxg/$tag_name" -f Dockerfile.deploy .
--- swarm.sh
+++ swarm.sh
@@ -2,8 +2,8 @@
 if [ $1 = 'swarm' ]; then 
   # Method1: run docker service by docker-stack.yml
   docker swarm init --advertise-addr $(hostname -i)
-  docker stack deploy -c docker-stack.yml mongo --build
+  docker stack deploy -c docker-stack.yml mongo 
 else
   # Method2: run docker service by docker-compose.yml
-  docker-compose -f docker-stack.yml up --build 
+  docker-compose -f docker-stack.yml up
 fi
