f5aa474305357056f3238593a397c812a8d04141
lqwangxg <lqwangxg@gmail.com>
2020-09-25 05:30:51 +0000

    commit

--- swarm.sh
+++ swarm.sh
@@ -1,9 +1,11 @@
 # check pattern before run service.
+stack_yml=docker-stack-mongo.yml
+stack_name=mongo
 if [ $1 = 'swarm' ]; then 
   # Method1: run docker service by docker-stack.yml
   docker swarm init --advertise-addr $(hostname -i)
-  docker stack deploy -c docker-stack.yml mongo 
+  docker stack deploy -c $stack_yml $stack_name 
 else
   # Method2: run docker service by docker-compose.yml
-  docker-compose -f docker-stack.yml up
+  docker-compose -f $stack_yml up
 fi
