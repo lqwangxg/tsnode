ab84b06ceda874497ce23aa34e07e138affed981
lqwangxg <lqwangxg@gmail.com>
2020-09-25 05:36:56 +0000

    commit

--- swarm.sh
+++ swarm.sh
@@ -3,6 +3,8 @@ stack_yml=docker-stack-mongo.yml
 stack_name=mongo
 if [ $1 = 'swarm' ]; then 
   # Method1: run docker service by docker-stack.yml
+  docker stack ls |grep $stack_name
+ 
   docker swarm init --advertise-addr $(hostname -i)
   docker stack deploy -c $stack_yml $stack_name 
 else
