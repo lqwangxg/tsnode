8bf5d38468e22ef869146cdb4a2fbe4b5fb78f67
lqwangxg <lqwangxg@gmail.com>
2020-09-25 04:32:50 +0000

    commit

--- swarm.sh
+++ swarm.sh
@@ -2,8 +2,8 @@
 if [ $1 = 'swarm' ]; then 
   # Method1: run docker service by docker-stack.yml
   docker swarm init --advertise-addr $(hostname -i)
-  docker stack deploy -c mongo-stack.yml mongo
+  docker stack deploy -c docker-stack.yml mongo --build
 else
   # Method2: run docker service by docker-compose.yml
-  docker-compose -f mongo-stack.yml up
+  docker-compose -f docker-stack.yml up --build 
 fi
